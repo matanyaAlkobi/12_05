f5ad32e0407372be1c8225c1367632b18dcf5dab
matanya alkobi <matanor123456@gmail.com>
2025-05-12 11:09:30 +0300

    Add project files.

---
+++ 11_05/11_05/Program.cs
@@ -0,0 +1,114 @@
+﻿
+
+//string[] new_list = { "rocket", "drone", "tank" };
+//List<int> houses = new List<int>();
+//int house_history = 0;
+//string wepen_history = "";
+//int number_of_house;
+//Random r = new Random();
+//int num = r.Next(1, 9);
+
+//for (int i = 0; i < num; i++)
+//{ 
+//    houses.Add(i);
+//}
+
+//Console.Write($"how many house to destroy between 0 to {num}");
+//int atack = int.Parse(Console.ReadLine());
+
+//for (int i = 0; atack > 0; atack--)
+//{
+//    string weppen = new_list[r.Next(0, 3)];
+//    int index = r.Next(0, atack);
+//    number_of_house = houses[index];
+//    houses.RemoveAt(index);
+
+//    if (i == 0)
+//    {
+//        house_history = number_of_house;
+//        wepen_history = weppen;
+//    }
+//    i++;
+
+//}
+//Console.WriteLine($"the first  house that  got atack {house_history}");
+//Console.WriteLine($"the first wepen that atck { wepen_history}");
+//foreach (int i in houses)
+//{
+//    Console.WriteLine(i);
+//}
+
+
+
+//question 1
+//List<string> names = new List<string> { "moshe", "adam","shy", "jakov", "ori"};
+//names.Add("shalom");
+//names.Add("gali");
+//names.Remove("adam");
+
+//for (int i = 0; i < names.Count; i++)
+//{
+//    Console.WriteLine($"{i} {names[i]}");
+//}
+
+
+//  question 2
+//Dictionary <string,int> my_dict = new Dictionary<string, int> { { "knif", 5 },
+//    { "cups", 6 },
+//    {"lemon" ,  2} };
+
+//my_dict["cups"] = 3;
+//foreach (string key in my_dict.Keys) 
+//{
+//    Console.WriteLine($"{key} = {my_dict[key]}");
+//}
+
+
+// question 3
+//List<Dictionary<string, string>> my_list = new List<Dictionary<string, string>>();
+//Dictionary<string, string> my_dict1 = new Dictionary<string, string> { { "names", "moshe"},
+//    {"email",  "kjnvjbs" },
+//    {"status", "online"},};
+//Dictionary<string, string> my_Dict2 = new Dictionary<string, string> { { "names", "alkobi"},
+//    {"email",  "@gmai.com" },
+//    {"status", "offline"},}; 
+//my_list.Add(my_dict1);
+//my_list.Add(my_Dict2);
+
+//foreach (var i in my_list)
+//{
+//    Console.WriteLine(i["email"]);
+//}
+
+
+
+
+//question 4
+//string[] user_names = { "moshe", "matan", "shoam" };
+//Console.WriteLine("enter a user name: ");
+//string user = Console.ReadLine();
+
+//if (user_names.Contains(user))
+//{
+//    Console.WriteLine(Array.IndexOf(user_names, user));
+//}
+//else
+//{
+//    Console.WriteLine("Username not registered.");
+//}
+
+
+
+// question 5
+List<string> tools1 = new List<string> { "recorder", "map", "notebook" };
+List<string> tools2 = new List<string> { "recorder", "map", };
+List<string> tools3 = new List<string> { "recorder",};
+
+Dictionary<string,List<string>> soliders =  new Dictionary<string,List<string>>();
+soliders.Add("Avi", tools1);
+
+
+
+
+
+
---
+++ 11_o5_0.2/11_o5_0.2/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_o5_0._2
+{
+    internal class Program
+    {
+        List<string> my_list = new List<string> ();
+
+        static void getAge()
+        {
+            int currentYear = 2025;
+            Console.Write("enter year of birth: ");
+            int yearOfBirth = int.Parse(Console.ReadLine());
+            Console.WriteLine($"your age  is {currentYear - yearOfBirth}");
+        }
+        static void getName()
+        {
+            Console.Write("what is your name? ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"hello {name}");
+        }
+
+        static void randomNumber()
+        {
+            Random r = new Random();
+            Console.WriteLine(r.Next());
+        }
+
+        static List<string> enter(string name)
+        {
+            my_list.Add(name);
+            return my_list;
+        }
+
+        //static List<string> exit(string name);
+        //{
+            
+        //}
+
+        static void Main(string[] args)
+        {
+            //    Console.WriteLine("\"Please choose from the menu:\\n\" +\r\n " +
+            //        "                 \"1 - We'll tell you what your age is\\n\" +\r\n " +
+            //        "                 \"2 - We'll greet you\\n\" +\r\n " +
+            //        "                 \"3 - We'll give you a random number\";");
+            //    int menu = int.Parse(Console.ReadLine());
+            //    switch (menu)
+            //    {
+            //        case 1:
+            //            getAge();
+            //            break;
+            //        case 2:
+            //            getName();
+            //            break;
+            //        case 3:
+            //            randomNumber();
+            //            break;
+
+            //    }
+
+
+
+
+        }
+    }
+}
